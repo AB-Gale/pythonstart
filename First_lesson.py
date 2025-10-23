8f24abd2c39f2deaee0e6601ac8b210cf95f3fc5
Uran <uran.aibashev.s@gmail.com>
2025-10-23 20:27:02 +0600

    First and Second lesson

---
+++ First_lesson.py
@@ -0,0 +1,68 @@
+# q = -20
+# w = -20
+# e = 257
+# r = 257
+# t = [1,2,3]
+# y = [1,2,3]
+# u = -5
+# i = -5 
+# print(q == w, q is w, e == r, e is r, t == y, t is y, u == i, u is i)
+# print(q is w)
+# s = "Привет!"
+# s_new = s.replace('и', 'ы')
+# print("Прывет!")
+# s1 = "Hello"
+# s2 = " "
+# s3 = "World!"
+# result = s1 + s2 + s3
+# result2 = s1 + s3
+# print(result2)
+# p = "Go"
+# result3 = p * 3
+# print(result3)
+# p1 = "Python"
+# print(p1[1:4])
+# p1.upper()
+# print(p1.upper())
+# print(p1.replace("t" "h", "X"))
+
+# Python Basic
+
+# a = int(5)
+# b = float(100.5)
+
+# image = '2 month ago gave'
+# image2 = "good"
+
+# question1 = input("Do you have new tire?")
+# print("Dealer's reponse", question1)
+# question2 = input("How much it is?")
+# print("New tire price", question2)
+# question3 = input("How you are paying?")
+# print("Payment method", question3)
+# question4 = input("Please provide and email for invoice")
+# print("email fro invoice", question4)
+
+# int - целые числа
+# float - не целые числа
+# str - text 
+
+# info = 'His name is Sam'
+# name = "John"
+# name = "name"
+# name = str("name")
+
+# num5 = 33.5
+# num1 = 5
+# print(num1)
+# type_of_num5 = type(num5)
+# print(type_of_num5)
+
+num1 = '5'
+num2 = 10
+num3 = 6.5
+dif = num2 - num3
+print(dif)
+
+dif2 = num2 - num1 
+print(dif2)
